7711eb512fd1bf4c685cd583d18b5565948f7487
lemonqsj <lemonqsj@163.com>
2018-04-11 21:47:11 +0800

    增加第三种移动的方式

---
+++ app/src/main/java/kc/com/scrollerview/widget/View_3.java
@@ -0,0 +1,21 @@
+package kc.com.scrollerview.widget;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @Auther: Lemon
+ * @Date: 2018/4/11 21:42 10
+ * @Describe: the infor of the class
+ */
+public class View_3 extends View {
+    public View_3(Context context) {
+        super(context);
+    }
+
+    public View_3(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
