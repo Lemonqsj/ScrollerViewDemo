993d4793c2faa04458e19f8abf8f6c9e9584d94a
lemonqsj <lemonqsj@163.com>
2018-04-15 10:43:47 +0800

    增加view的生命周期方法

--- app/src/main/java/kc/com/scrollerview/MainActivity.java
+++ app/src/main/java/kc/com/scrollerview/MainActivity.java
@@ -2,12 +2,15 @@ package kc.com.scrollerview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.TextView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "View_4";
     @BindView(R.id.text)
     TextView mText;
 
@@ -16,9 +19,63 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
+        Log.d(TAG, "onCreate: activity");
 
         mText.setText("test butterknife");
 
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: activity");
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+
+        Log.d(TAG, "onResume: activity");
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+
+        Log.d(TAG, "onStop: activity");
+        super.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+
+        Log.d(TAG, "onPause: activity");
+        super.onPause();
+    }
+
+    @Override
+    public void onDetachedFromWindow() {
+
+        Log.d(TAG, "onDetachedFromWindow: activity");
+        super.onDetachedFromWindow();
+    }
+
+    @Override
+    public void onAttachedToWindow() {
+
+        Log.d(TAG, "onAttachedToWindow: activity");
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        Log.d(TAG, "onWindowFocusChanged: activity");
+        super.onWindowFocusChanged(hasFocus);
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        Log.d(TAG, "onDestroy: activity");
+        super.onDestroy();
     }
 }
--- app/src/main/java/kc/com/scrollerview/widget/View_4.java
+++ app/src/main/java/kc/com/scrollerview/widget/View_4.java
@@ -1,10 +1,13 @@
 package kc.com.scrollerview.widget;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
+
 
 /**
  * @Auther: Lemon
@@ -12,14 +15,103 @@ import android.view.View;
  * @Describe: the infor of the class
  */
 public class View_4 extends View {
+
+/**
+  * Desc：自定义view的生命周期
+  *   1: 创建creat：通常有两种方式，下面有四个构造方法，平时也只是能用到两个，最后一个四个参数的构造方法没有用到过
+ *          1. 一个参数的构造，在代码中new 的时候调用；
+ *          2. 两个参数的构造，在XML中使用的时候回调用
+ *          3. 三个参数的构造一般是在获取一些自定义属性的时候调用的到
+ *    2： 加载view：当加载完成的时候会调用onFinshInflate，创建和加载完成是相对应的；
+ *    3： 进入activity的创建，开始，显示，加载到window上面，当activity加载完成之后会调用view 的onAttachToWindow
+ *    4:  当attach完成之后会onMeasure和onLayout
+ *    5:  布局和测量完成之后activity就会获取到焦点，这时候activity和view的都会被调用onWindowFocusChanged，并且获取到焦点hasWindowFocus
+ *    6： 获取到
+ *
+  *
+  *
+  */
+
+    private static final String TAG = "View_4";
     public View_4(Context context) {
         super(context);
+
+
+
+        Log.d(TAG, "View_4: 一个参数的构造");
+        
+        
     }
+
+
 
     public View_4(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        Log.d(TAG, "View_4: 两个参数的构造");
     }
 
+    @Override
+    protected void onFinishInflate() {
+
+        Log.d(TAG, "onFinishInflate: ");
+        super.onFinishInflate();
+    }
+
+
+    @Override
+    protected void onAttachedToWindow() {
+
+        Log.d(TAG, "onAttachedToWindow: ");
+        super.onAttachedToWindow();
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        Log.d(TAG, "onMeasure: ");
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+
+    @Override
+    protected void onLayout(boolean changed, int left, int top, int right, int bottom) {
+
+        Log.d(TAG, "onLayout: ");
+        super.onLayout(changed, left, top, right, bottom);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        Log.d(TAG, "onDraw: ");
+        super.onDraw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        Log.d(TAG, "onSizeChanged: ");
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+
+        Log.d(TAG, "onWindowFocusChanged: ");
+        super.onWindowFocusChanged(hasWindowFocus);
+    }
+
+    @Override
+    public boolean hasWindowFocus() {
+
+        Log.d(TAG, "hasWindowFocus: ");
+        return super.hasWindowFocus();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        Log.d(TAG, "onDetachedFromWindow: ");
+        super.onDetachedFromWindow();
+    }
 
     private int lastX, lastY;
 
